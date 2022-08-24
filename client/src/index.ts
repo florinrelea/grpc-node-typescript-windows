68700aa3ea34b255bcce22a0b4d28dc19d22b8cb
florinrelea <florinrelea65@gmail.com>
2022-08-24 17:11:02 +0300

    How to specify deadline

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,8 +3,10 @@ import { usersServiceClient } from './rpc-connection'
 
 async function main() {
   usersServiceClient.GetUser(new Users.GetUserRequest({
-   id: 'the-computer-exert'
-  }), (error, response) => {
+    id: 'the-computer-exert'
+  }), {
+    deadline: new Date(Date.now() + (30 * 1000)) // Deadline in 30 seconds
+  }, (error, response) => {
     if (error) {
       console.error(error)
       return
